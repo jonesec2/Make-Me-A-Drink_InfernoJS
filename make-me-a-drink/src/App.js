d78b297ef5269c8ec17b9a689ef3b11f35385b45
pb-totoro <jonesec2@gmail.com>
2020-06-17 11:33:36 -0400

    created inferno app, and started working on the background

---
+++ make-me-a-drink/src/App.js
@@ -0,0 +1,21 @@
+import {  Component } from 'inferno';
+import Logo from './logo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Logo width="80" height="80" />
+          <p>Welcome to my freaking app</p>
+          <p>
+            Sup you duders?!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
