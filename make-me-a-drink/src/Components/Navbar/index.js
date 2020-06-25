528e61cfa04cd2cabdccd6cac273b05ce4be690c
pb-totoro <jonesec2@gmail.com>
2020-06-24 23:19:37 -0400

    more nav styling, ok I'm stopping for real

--- make-me-a-drink/src/Components/Navbar/index.js
+++ make-me-a-drink/src/Components/Navbar/index.js
@@ -1,5 +1,5 @@
 // import {useState, useEffect} from "inferno";
-import {Link} from "inferno-router";
+import { Link } from "inferno-router";
 import "./css/style.css";
 
 
@@ -10,17 +10,24 @@ export default function Navbar() {
    //    setPath(window.location.pathname);
    // }, [path])
 
+   //write onClick event for the "Dark Mode" button
+
    return (
       <nav className="nav test">
          <Link id="id" className="title" to="/drink"><h4 className="lobster">Home</h4></Link>
          <div className="list-div">
             <ul className="nav-list">
-               <li>
-                  <Link id="id2" className="nav-item" to="/saved"><a>Saved Drinks</a></Link>
+               <li className="nav-item">
+                  <Link id="id2" className="vollkorn" to="/saved"><a>Saved Drinks</a></Link>
                </li>
-               <li>
-                  <button id="id3" className="nav-item"><a>Dark Mode</a></button>
+               <li className="nav-item">
+                  <Link id="id3" className="vollkorn"><a>Dark Mode</a></Link>
                </li>
+               <form class="form">
+               <input className="vollkorn smooth" type="search" placeholder="Drink Search"
+                  aria-label="Mix Drink Search"/>
+                  <button className="vollkorn smooth" type="submit">Search</button>
+            </form>
             </ul>
          </div>
       </nav>
